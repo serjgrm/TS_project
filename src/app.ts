c32eb911a8f0532b8607d1150bdfd08a69978c49
Serhii <grammas56@gmail.com>
2023-03-02 14:01:16 +0200

    Prompt_Input

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import { PromptService } from "./core/prompt/prompt.service";
+export class App {
+	async run() {
+		const res = await(new PromptService()).input<number>('Число', 'number')
+		console.log(res);
+	}
+}
+const app = new App();
+app.run();
